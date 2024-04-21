097c085af293b93f93dc02ef871e48c166898856
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-21 09:49:31 -0300

    Sistema de Hospedagem

--- SistemaHospedagem/Program.cs
+++ SistemaHospedagem/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SistemaHospedagem
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Pessoa> hospedes = new List<Pessoa>();
+
+            Pessoa p1 = new Pessoa(nome: "Hospede 1");
+            Pessoa p2 = new Pessoa(nome: "Hospede 2");
+
+            hospedes.Add(p1);
+            hospedes.Add(p2);
+
+            Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 50);
+
+            Reserva reserva = new Reserva(diasReservados: 10);
+            reserva.CadastrarSuite(suite);
+            reserva.CadastrarHospedes(hospedes);
+
+            Console.WriteLine($"Hospedes: {reserva.ObterQuantidadeHospedes()}");
+            Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+        }
+    }
+}
